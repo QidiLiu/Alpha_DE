ea9642bd9d8ef19d56fdda91ff8e598a91d3c5ac
Qidi Liu <adi0304@hotmail.com>
2020-05-02 23:31:45 +0200

    字幕提取时不再保留换行符

--- 字幕提取.py
+++ 字幕提取.py
@@ -17,7 +17,7 @@ if ass_file is not None:
     with open(ass_file.name.split(ass_file.name.split('/')[-1])[0]+'text_'+ass_file.name.split('/')[-1].split('.')[0]+'.txt', 'w', encoding='UTF-8') as file_write:
         for line in lines:
             if match('Dialogue:', line) is not None:
-                TEXT = ''.join(list(line)[50:])+','
+                TEXT = ''.join(list(line.strip('\n'))[54:])
             file_write.write(TEXT)
 
 _exit(0)
